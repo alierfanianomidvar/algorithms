ffd9e8800e68e55e16a9842045725b2399c69041
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-08-07 11:07:46 +0200

    Add - the solution

--- src/java/validate_binary_search_tree/ValidateBinarySearchTree.java
+++ src/java/validate_binary_search_tree/ValidateBinarySearchTree.java
@@ -1,10 +1,22 @@
 package src.java.validate_binary_search_tree;
 
 public class ValidateBinarySearchTree {
-
     public boolean isValidBST(TreeNode root) {
+        return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
 
+    private boolean isValidBSTHelper(TreeNode root, long minValue, long maxValue) {
 
-        return true;
+        if (root == null) return true;
+        else if (root.val <= minValue || root.val >= maxValue) return false;
+        return isValidBSTHelper(
+                root.left,
+                minValue,
+                root.val)
+                &&
+                isValidBSTHelper(
+                        root.right,
+                        root.val,
+                        maxValue);
     }
 }
