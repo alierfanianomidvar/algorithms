2dd2397215777f9ce5bb11926e9216856faf5a7e
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-08-08 11:32:29 +0200

    Add - the solution.

--- src/java/valid_parentheses/ValidParentheses.java
+++ src/java/valid_parentheses/ValidParentheses.java
@@ -1,11 +1,25 @@
 package src.java.valid_parentheses;
 
+import java.util.LinkedList;
+
 public class ValidParentheses {
 
     public boolean solution(String s) {
 
-
-        return true;
+        LinkedList<Character> characters = new LinkedList<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(!characters.isEmpty()){
+                if ((int)(characters.getLast() - c) <=1
+                        && (int)(characters.getLast() - c) >= -2
+                        && characters.getLast() != c) {
+                    characters.removeLast();
+                } else {
+                    characters.add(c);
+                }
+            } else characters.push(c);
+        }
+        return characters.isEmpty();
     }
 
 }
