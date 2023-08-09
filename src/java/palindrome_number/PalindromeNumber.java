cd5fb955eaa45e3ed0ae975555d2ae8719742de5
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-08-09 12:34:31 +0200

    Add the solution.

--- src/java/palindrome_number/PalindromeNumber.java
+++ src/java/palindrome_number/PalindromeNumber.java
@@ -1,4 +1,25 @@
 package src.java.palindrome_number;
 
+import java.util.ArrayList;
+
 public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        int original = x;
+        int reversNumber = 0;
+        if(x < 0){
+            return false;
+        } else if (x < 10 && x >= 0) {
+            return true;
+        } else if (x % 10 == 0) {
+            return false;
+        } else {
+            while (x > 0) {
+                int number = x % 10;
+                reversNumber = reversNumber * 10 + number;
+                x /= 10;
+            }
+            return original == reversNumber;
+        }
+    }
 }
