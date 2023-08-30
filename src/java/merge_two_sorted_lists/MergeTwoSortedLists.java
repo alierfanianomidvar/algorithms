f102f7c31a1692306eefdf59c3a744133a53f37f
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-08-30 10:49:43 +0200

    Add - solution to the alg problem of mege two sorted lists.

--- src/java/merge_two_sorted_lists/MergeTwoSortedLists.java
+++ src/java/merge_two_sorted_lists/MergeTwoSortedLists.java
@@ -1,8 +1,22 @@
 package src.java.merge_two_sorted_lists;
 
 public class MergeTwoSortedLists {
-    public void mergeTwoLists(ListNode list1, ListNode list2) {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
+        ListNode newList = new ListNode();
 
+        if(list1 == null){
+            return list2;
+        } else if (list2 == null){
+            return list1;
+        } else if( list1.val < list2.val){
+            newList.val = list1.val;
+            newList.next = mergeTwoLists(list1.next,list2);
+        } else {
+            newList.val = list2.val;
+            newList.next = mergeTwoLists(list1,list2.next);
+        }
+
+        return newList;
     }
 }
