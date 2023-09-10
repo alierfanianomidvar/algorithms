f933797830cc210aa53908d1430011228be323a3
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-09-10 14:43:46 +0200

    Add the best solution.

--- src/java/FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/java/FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -5,7 +5,6 @@ public class FindTheIndexOfTheFirstOccurrenceInAString {
 
     public int Solution(String haystack, String needle) {
 
-
         // This is the first way that solve this problem
         /*
         int x = 0, y = 0;
@@ -31,7 +30,28 @@ public class FindTheIndexOfTheFirstOccurrenceInAString {
                 y++;
         }*/
 
-        return 0;
+        int haystackLength = haystack.length();
+        int needleLength = needle.length();
+
+        // Check for edge cases
+        if (needleLength == 0) return 0;
+        if (haystackLength < needleLength) return -1;
+
+        for (int i = 0; i <= haystackLength - needleLength; i++) {
+            int j;
+            for (j = 0; j < needleLength; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break; // mismatch, move to the next position in haystack
+                }
+            }
+
+            // If we reached the end of the needle, it means we found a match.
+            if (j == needleLength) {
+                return i;
+            }
+        }
+
+        return -1; // Needle not found in haystack
     }
 
 }
