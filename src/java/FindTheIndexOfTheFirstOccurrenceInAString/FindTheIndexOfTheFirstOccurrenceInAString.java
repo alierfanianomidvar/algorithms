b719a5a2096100e0da1ee9f7f75701774f43a21f
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-09-10 14:40:25 +0200

    Add my first solution of the problem, but not a good one.

--- src/java/FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/java/FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -5,6 +5,32 @@ public class FindTheIndexOfTheFirstOccurrenceInAString {
 
     public int Solution(String haystack, String needle) {
 
+
+        // This is the first way that solve this problem
+        /*
+        int x = 0, y = 0;
+
+        if(needle.length() > haystack.length()){
+            return -1;
+        }
+        while (true) {
+
+            if (needle.charAt(x) != haystack.charAt(y)) {
+                y = y - x;
+                x = 0;
+            } else {
+                x++;
+            }
+            if (x > needle.length()-1)
+                return y+1-x;
+            else if ((x == 0 && y == haystack.length()-1) || x>y+1)
+                return -1;
+            else if (y == haystack.length()-1)
+                return -1;
+            else
+                y++;
+        }*/
+
         return 0;
     }
 
