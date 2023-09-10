35fc7fb30c4e7eb42148b2d13a15d64fe0b510ba
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-09-10 14:49:01 +0200

    Add the description of the problem.

--- src/java/FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/java/FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,3 +1,27 @@
+/**
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: haystack = "sadbutsad", needle = "sad"
+ * Output: 0
+ * Explanation: "sad" occurs at index 0 and 6.
+ * The first occurrence is at index 0, so we return 0.
+ * Example 2:
+ * <p>
+ * Input: haystack = "leetcode", needle = "leeto"
+ * Output: -1
+ * Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= haystack.length, needle.length <= 104
+ * haystack and needle consist of only lowercase English characters.
+ */
+
 package src.java.FindTheIndexOfTheFirstOccurrenceInAString;
 
 public class FindTheIndexOfTheFirstOccurrenceInAString {
