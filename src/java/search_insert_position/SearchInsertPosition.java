8d991a21fc59848d6e04f563bf07ec48ea4c9902
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-09-27 17:14:42 +0200

    Add the description of the problem
    Also fixing the a part of algo.

--- src/java/search_insert_position/SearchInsertPosition.java
+++ src/java/search_insert_position/SearchInsertPosition.java
@@ -1,3 +1,32 @@
+/**
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ *
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ * Example 2:
+ *
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ * Example 3:
+ *
+ * Input: nums = [1,3,5,6], target = 7
+ * Output: 4
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * nums contains distinct values sorted in ascending order.
+ * -104 <= target <= 104
+ * */
+
 package src.java.search_insert_position;
 
 public class SearchInsertPosition {
@@ -9,10 +38,8 @@ public class SearchInsertPosition {
             return size;
         } else {
             for (int i = 0; i < size; i++) {
-                 if(nums[i]> target){
+                 if(nums[i]>=target){
                      return i-1;
-                 } else if( nums[i] == target){
-                     return i;
                  }
             }
             return size;
