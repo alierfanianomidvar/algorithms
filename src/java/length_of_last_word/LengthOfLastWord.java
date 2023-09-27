8e183104222621f840b364cb310c04dfbe1672a9
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-09-27 17:45:37 +0200

    Add the algorithm of length of last word.

--- src/java/length_of_last_word/LengthOfLastWord.java
+++ src/java/length_of_last_word/LengthOfLastWord.java
@@ -2,6 +2,18 @@ package src.java.length_of_last_word;
 
 public class LengthOfLastWord {
     public int Solution(String s) {
-        return 0;
+        int size = s.length();
+        int length = 0;
+        boolean weSeenTheWord = false;
+
+        for (int i = size - 1; i >=0 ; i--) {
+            if(s.charAt(i) != ' '){
+                length++;
+                weSeenTheWord = true;
+            } else if(s.charAt(i) == ' ' && weSeenTheWord){
+                return length;
+            }
+        }
+        return length;
     }
 }
