986af006ad89f71086539964bb4207b22f2a129b
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-09-27 13:59:38 +0200

    Add the sulotion of the problem.

--- src/java/search_insert_position/SearchInsertPosition.java
+++ src/java/search_insert_position/SearchInsertPosition.java
@@ -1,4 +1,21 @@
 package src.java.search_insert_position;
 
 public class SearchInsertPosition {
+
+    public int solution(int[] nums, int target) {
+
+        int size = nums.length;
+        if( target > nums[size-1]){
+            return size;
+        } else {
+            for (int i = 0; i < size; i++) {
+                 if(nums[i]> target){
+                     return i-1;
+                 } else if( nums[i] == target){
+                     return i;
+                 }
+            }
+            return size;
+        }
+    }
 }
