1aabe48c8e55cf7a0392a04684f5fdb1d2330586
Ali Erfanian (I) <ali.erfanian@punchtorino.com>
2023-11-06 21:00:31 +0100

    Add: comment to the code.
    Also upgrade the java vserion of project to the 21.

--- src/java/add_binary/AddBinary.java
+++ src/java/add_binary/AddBinary.java
@@ -22,43 +22,49 @@ package src.java.add_binary;
 public class AddBinary {
 
     public String solution(String a, String b) {
-        int sizeStringA = a.length();
-        int sizeStringB = b.length();
-        int carry = 0;
-        
+        // Variables to store the length of the strings
+        int sizeStringA = a.length() - 1; // index should start from the last character
+        int sizeStringB = b.length() - 1; // index should start from the last character
+        int carry = 0; // This will hold the carry over when adding two binary digits
+
+        // StringBuilder is a mutable sequence of characters. Used here for efficient string concatenation
         StringBuilder output = new StringBuilder();
 
-        while(sizeStringA >= 0 || sizeStringB >= 0){
-            int sum = carry;
-            if(sizeStringA >= 0 ){
-                sum += a.CharAt(sizeStringA) - '0';
-            }
-            if(sizeStringB >= 0 ){
-                sum += b.CharAt(sizeStringB) - '0';
+        // Continue looping until both strings have been fully traversed
+        while (sizeStringA >= 0 || sizeStringB >= 0) {
+            int sum = carry; // Start with the current carry value
+
+            // If we haven't gone past the beginning of String a, add the digit to sum
+            if (sizeStringA >= 0) {
+                sum += a.charAt(sizeStringA) - '0'; // Convert char to int and add it to sum
+                sizeStringA--; // Move to the next character
             }
 
-            if(sum == 0 || sum == 1){
-                output.append(sum);
-                carry = 0;
-            } else if(sum == 2) {
-                output.append("0");
-                carry = 1;
+            // If we haven't gone past the beginning of String b, add the digit to sum
+            if (sizeStringB >= 0) {
+                sum += b.charAt(sizeStringB) - '0'; // Convert char to int and add it to sum
+                sizeStringB--; // Move to the next character
+            }
+
+            // Append the right digit to output and update carry
+            if (sum <= 1) {
+                output.append(sum); // If the sum is 0 or 1, append it directly
+                carry = 0; // No carry if sum is less than 2
+            } else if (sum == 2) {
+                output.append("0"); // If the sum is 2, append 0 and carry over 1
+                carry = 1; // Set carry to 1
             } else {
-                output.append("1");
-                carry = 1;
+                output.append("1"); // If the sum is 3 (1+1+carry), append 1 and carry over 1
+                carry = 1; // Set carry to 1
             }
-            sizeStringA --;
-            sizeStringB --;
         }
 
-        if(carry == 1){
-            output.append("1")
+        // If there's still a carry after the main loop, append it
+        if (carry == 1) {
+            output.append("1");
         }
-        return output.reverse().tostring(); 
+
+        // Since we've been appending digits in reverse order, we need to reverse the StringBuilder contents
+        return output.reverse().toString();
     }
-
-   
-    
-
-   
 }
