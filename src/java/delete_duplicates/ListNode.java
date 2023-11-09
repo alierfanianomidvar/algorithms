4c41d0799c7760cec834319a0e4943a467a209f5
Ali Erfanian (I) <ali.erfanian@punchtorino.com>
2023-11-09 13:39:21 +0100

    Initial delete duplicatese in linklist

---
+++ src/java/delete_duplicates/ListNode.java
@@ -0,0 +1,9 @@
+package src.java.delete_duplicates;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ src/java/delete_duplicates/deleteDuplicates.java
@@ -0,0 +1,31 @@
+/*
+* Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+
+
+Example 1:
+Input: head = [1,1,2]
+Output: [1,2]
+*
+Example 2:
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+*
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+*
+* */
+package src.java.delete_duplicates;
+
+import java.util.List;
+
+public class deleteDuplicates {
+
+    public ListNode Solution(ListNode head) {
+
+        return new ListNode();
+    }
+}
