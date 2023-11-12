221b138370b49a35c663c156f2810c92b74b7116
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-11-12 14:41:00 +0100

    Initial commit of binary tree in order traversal.

---
+++ src/java/Binary_Tree_Inorder_Traversal/BinaryTreeInorderTraversal.java
@@ -0,0 +1,10 @@
+package src.java.Binary_Tree_Inorder_Traversal;
+
+import java.util.LinkedList;
+import java.util.List;
+public class BinaryTreeInorderTraversal {
+
+    public List<Integer> Solution(TreeNode root) {
+        return new LinkedList<>();
+    }
+}
