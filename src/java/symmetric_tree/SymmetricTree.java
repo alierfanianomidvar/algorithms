cdbb91588e5802176cb4b428527ae1ee40246342
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-11-16 22:54:08 +0100

    Add: the iterative solution for the symmetric tree.

--- src/java/symmetric_tree/SymmetricTree.java
+++ src/java/symmetric_tree/SymmetricTree.java
@@ -24,6 +24,9 @@ The number of nodes in the tree is in the range [1, 1000].
 
 package src.java.symmetric_tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SymmetricTree {
 
     public boolean solution(TreeNode root) {
@@ -44,5 +47,28 @@ public class SymmetricTree {
 
         // the far left side must be same as the far right and right side of left :) and left side of right must be same.
         return recursive(left.left, right.right) && recursive(left.right, right.left);
+    }
+
+    public boolean iterative(TreeNode node){
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(node.left);
+        queue.add(node.right);
+
+        while (!queue.isEmpty()){
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+
+            if(node1 == null && node2 == null)
+                continue;
+            else if(node1 == null || node2 == null || node1.val != node2.val)
+                return false;
+
+            queue.add(node1.left);
+            queue.add(node2.right);
+            queue.add(node1.right);
+            queue.add(node2.left);
+        }
+        return true;
     }
 }
