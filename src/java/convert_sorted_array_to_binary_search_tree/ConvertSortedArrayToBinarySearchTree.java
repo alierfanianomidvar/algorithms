c129bb80deb35794158183c7b9b59e5d1639f849
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-11-20 22:23:33 +0100

    Initial commit  ConvertSortedArrayToBinarySearchTree.

---
+++ src/java/convert_sorted_array_to_binary_search_tree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,9 @@
+package src.java.convert_sorted_array_to_binary_search_tree;
+
+public class ConvertSortedArrayToBinarySearchTree {
+
+    public TreeNode solution(int[] nums) {
+
+        return new TreeNode();
+    }
+}
