0c2c167657046764a0730e3cfa35d3f20a4a5819
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-01-09 13:53:27 +0100

    Initial commit for path sum.

---
+++ src/java/path_sum/PathSum.java
@@ -0,0 +1,14 @@
+package src.java.path_sum;
+
+import src.java.Binary_Tree_Inorder_Traversal.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathSum {
+
+    public boolean solution(TreeNode root) {
+        return false;
+    }
+
+}
