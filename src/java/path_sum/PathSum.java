25945b857f46e484e01b461480be6f008bfe3773
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-01-11 13:45:32 +0100

    Add: the solution of the path sum.

--- src/java/path_sum/PathSum.java
+++ src/java/path_sum/PathSum.java
@@ -1,14 +1,53 @@
 package src.java.path_sum;
 
-import src.java.Binary_Tree_Inorder_Traversal.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PathSum {
 
-    public boolean solution(TreeNode root) {
-        return false;
+    public boolean solution(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        boolean rightSide = false;
+        boolean leftSide = false;
+
+        if (root.right == null && root.left == null) {
+            return root.val == targetSum;
+        }
+
+        if(root.right != null){
+            rightSide = r(root.right, root.val, targetSum);
+        }
+
+        if(root.left != null){
+            leftSide = r(root.left, root.val, targetSum);
+        }
+
+        return  rightSide || leftSide;
+    }
+
+    public boolean r(TreeNode root, int nodeValue, int targetSum) {
+
+        boolean rightSide = false;
+        boolean leftSide = false;
+        nodeValue += root.val;
+
+        if (root.right == null && root.left == null) {
+            return nodeValue == targetSum;
+        }
+
+        if(root.right != null){
+            rightSide = r(root.right, nodeValue, targetSum);
+        }
+
+        if(root.left != null){
+            leftSide = r(root.left, nodeValue, targetSum);
+        }
+
+        return rightSide || leftSide;
     }
 
 }
