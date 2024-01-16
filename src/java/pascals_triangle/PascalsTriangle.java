daa988e88985e3f8674e760f8fbcf533aec03a5a
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-01-16 17:19:39 +0100

    initial commit of pascal triangle.

---
+++ src/java/pascals_triangle/PascalsTriangle.java
@@ -0,0 +1,14 @@
+package src.java.pascals_triangle;
+
+import src.java.minimum_depth_of_binary_tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+
+    public List<List<Integer>> solution(int numRows) {
+        List<List<Integer>> pascalsTriangle = new ArrayList<>();
+        return pascalsTriangle;
+    }
+}
