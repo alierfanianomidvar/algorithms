b319c8ed8915a80f7b653be7c5d840fdbbd1f487
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-01-26 20:10:00 +0100

    Add: solution of the pascals trinagle.

--- src/java/pascals_triangle/PascalsTriangle.java
+++ src/java/pascals_triangle/PascalsTriangle.java
@@ -9,6 +9,20 @@ public class PascalsTriangle {
 
     public List<List<Integer>> solution(int numRows) {
         List<List<Integer>> pascalsTriangle = new ArrayList<>();
+
+        for (int n = 0; n < numRows; n++) {
+            List<Integer> row = new ArrayList<>();
+            int value = 1; // Starting with 1 as nC0 is always 1
+            row.add(value);
+
+            for (int k = 1; k <= n; k++) {
+                value = value * (n - k + 1) / k; // Calculate next value in the row
+                row.add(value);
+            }
+
+            pascalsTriangle.add(row);
+        }
+
         return pascalsTriangle;
     }
 }
