5bb20c1d58b4d19d06c4c2d1d51463465b477d2f
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-02-05 11:38:21 +0100

    Add: solution for pascal triangle II qustion.

--- src/java/pascals_triangle/PascalTriangleII.java
+++ src/java/pascals_triangle/PascalTriangleII.java
@@ -6,7 +6,20 @@ import java.util.List;
 public class PascalTriangleII {
 
     public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> pascalsTriangle = new ArrayList<>();
 
-        return new ArrayList<>();
+        for (int n = 0; n <= rowIndex; n++) {
+            List<Integer> row = new ArrayList<>();
+            long value = 1; // Starting with 1 as nC0 is always 1
+            row.add((int) value);
+
+            for (int k = 1; k <= n; k++) {
+                value = value * (n - k + 1) / k; // Calculate next value in the row
+                row.add((int) value);
+            }
+            pascalsTriangle.add(row);
+        }
+
+        return pascalsTriangle.get(rowIndex);
     }
 }
