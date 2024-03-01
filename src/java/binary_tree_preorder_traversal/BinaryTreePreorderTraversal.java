b8dc9cc946d076692d265787a1766ef1892d7321
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-03-01 11:02:10 +0100

    Initial commit of     public List<Integer> preorderTraversal(TreeNode root) {

---
+++ src/java/binary_tree_preorder_traversal/BinaryTreePreorderTraversal.java
@@ -0,0 +1,13 @@
+package src.java.binary_tree_preorder_traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+
+        return new ArrayList<>();
+    }
+
+}
