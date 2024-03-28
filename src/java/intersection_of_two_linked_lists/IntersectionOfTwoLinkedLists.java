79ff803eb9e54103060c2a6c08e8b68279b10015
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-03-28 12:57:52 +0100

    Add: Intersection of Two Linked Lists file.

---
+++ src/java/intersection_of_two_linked_lists/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,9 @@
+package src.java.intersection_of_two_linked_lists;
+
+
+public class IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        return new ListNode(1);
+    }
+}
