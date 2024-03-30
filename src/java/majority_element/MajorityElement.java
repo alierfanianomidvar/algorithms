b3aaead3ccb598890875eb685adc70819caac032
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-03-30 14:55:25 +0100

    Add: the Boyer-Moore Majority Vote Algorithm for the majority element.

--- src/java/majority_element/MajorityElement.java
+++ src/java/majority_element/MajorityElement.java
@@ -31,4 +31,43 @@ public class MajorityElement {
     /*This is not a fast way to find the majority element, we can use another algo just for the majority*/
 
 
+    public int majorityElementSecondAlgo(int[] nums) {
+
+        int candidate = nums[0];
+        int count = 0;
+
+        // find the best candidate - >
+        for (int i = 1; i < nums.length; i++) {
+            if (candidate == nums[i]) {
+                count++;
+            } else if (count == 0) {
+                candidate = nums[i];
+            } else {
+                count--;
+            }
+        }
+
+        //TODO - > here we can just return the candidate;
+
+        // see how many times we see the candidate in the list - >
+        count = 0;
+        for (int num : nums) {
+            if (candidate == num) {
+                count++;
+            }
+        }
+
+        // The majority element is the element that appears more than ⌊n / 2⌋ times. ->
+        if (count > nums.length / 2) {
+            return candidate;
+        } else {
+            return -1;
+        }
+
+        /* we can just return the candidate and never check it becasue int the text of the question it says that we must assume
+         * that majority exists.
+         *
+         * this is Boyer-Moore Majority Vote Algorithm.
+         * */
+    }
 }
