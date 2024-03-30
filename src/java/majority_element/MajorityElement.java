7f678ecbdf31392608a6a43458f0b9f2f1712d2f
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-03-30 14:46:37 +0100

    Add: the majority element first algo
    using hashmap.

--- src/java/majority_element/MajorityElement.java
+++ src/java/majority_element/MajorityElement.java
@@ -1,9 +1,34 @@
 package src.java.majority_element;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MajorityElement {
 
     public int majorityElement(int[] nums) {
 
-        return 0;
+        Map<Integer, Integer> hashMap = new HashMap<>(); // using hashmap we can save the number of occurrence in the hash too
+
+        int maxValue = Integer.MIN_VALUE;
+        int maxKey = 0;
+
+        for (int num : nums) {
+            if (hashMap.containsKey(num)) {
+                hashMap.compute(num, (key, value) -> value + 1);
+            } else {
+                hashMap.put(num, 1);
+            }
+
+            if (hashMap.get(num) > maxValue) {
+                maxValue = hashMap.get(num);
+                maxKey = num;
+            }
+        }
+
+        return maxKey;
     }
+
+    /*This is not a fast way to find the majority element, we can use another algo just for the majority*/
+
+
 }
