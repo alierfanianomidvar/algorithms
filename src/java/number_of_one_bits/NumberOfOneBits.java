972aa7694311bb2d9b1f87ba0bd67a72dac250eb
alierfanian <ali.erfanian@punchtorino.com>
2024-05-06 16:55:11 +0200

    Add: solution of number of one bite.

---
+++ src/java/number_of_one_bits/NumberOfOneBits.java
@@ -0,0 +1,18 @@
+package src.java.number_of_one_bits;
+
+public class NumberOfOneBits {
+
+    public int hammingWeight(int n) {
+
+        int counter = 0;
+
+        while (n > 0) {
+            if (n % 2 == 1) {
+                counter++;
+            }
+            n = n >>= 1; // this make code faster;
+        }
+
+        return counter;
+    }
+}
