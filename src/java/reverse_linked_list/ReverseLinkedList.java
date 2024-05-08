4ab489728015986dccbc2184aa784965fead473b
alierfanian <ali.erfanian@punchtorino.com>
2024-05-08 16:50:32 +0200

    Add: the solution of the reverselinked list
    beats 100 of the java users.

--- src/java/reverse_linked_list/ReverseLinkedList.java
+++ src/java/reverse_linked_list/ReverseLinkedList.java
@@ -1,5 +1,8 @@
 package src.java.reverse_linked_list;
 
+import javax.print.DocFlavor;
+import java.util.List;
+
 class ListNode {
     int val;
 
@@ -14,5 +17,20 @@ class ListNode {
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
 
+        if(head == null){
+            return head;
+        }
+        return r(null, head);
+    }
+
+    private ListNode r(ListNode p, ListNode node) {
+
+        if (node.next != null) {
+            ListNode newHead = r(node, node.next);
+            node.next = p;
+            return newHead;
+        }
+        node.next = p;
+        return node;
     }
 }
