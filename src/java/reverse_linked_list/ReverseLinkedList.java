a5d1424aa124b262fe90a8070f98a5673e1094d4
alierfanian <ali.erfanian@punchtorino.com>
2024-05-08 16:35:54 +0200

    Add: the reverse link list problem file.

---
+++ src/java/reverse_linked_list/ReverseLinkedList.java
@@ -0,0 +1,18 @@
+package src.java.reverse_linked_list;
+
+class ListNode {
+    int val;
+
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+
+    }
+}
