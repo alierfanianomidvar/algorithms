71b3da6071ba2b2c7b7716f7944dda2b70634eb7
ali erfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-05-12 14:20:56 +0200

    Add: sorted way for the contains duplicate problem.

--- src/java/contains_duplicate/ContainsDuplicate.java
+++ src/java/contains_duplicate/ContainsDuplicate.java
@@ -11,7 +11,7 @@ public class ContainsDuplicate {
         Set<Integer> visited = new HashSet<>();
 
         for (int num : nums) {
-            if(visited.contains(num)){
+            if (visited.contains(num)) {
                 return true;
             } else {
                 visited.add(num);
@@ -20,4 +20,20 @@ public class ContainsDuplicate {
 
         return false;
     }
+
+    // we can solve with another way - >
+    // we can sort the list first than move throw the list inside of a for-loop
+    /**
+     * public boolean containsDuplicate(int[] nums) {
+     *         Arrays.sort(nums);
+     *         for (int i = 1; i < nums.length; i++) {
+     *             if (nums[i] == nums[i - 1]) {
+     *                 return true;
+     *             }
+     *         }
+     *         return false;
+     *     }
+     *     */
+    // this nlog(n) and the main way we want is n but this sorted way is more memory efficient.
+
 }
