bbbe6ea5bc193c77b166d2f0745b9c6b09ff38d6
alierfanian <ali.erfanian@punchtorino.com>
2024-06-05 10:59:05 +0200

    Add: the palindrome linklist problem with my first solution
    reverse the list and check the 2 link list .

---
+++ src/java/palindrome_linked_list/PalindromeLinkedList.java
@@ -0,0 +1,53 @@
+package src.java.palindrome_linked_list;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+
+        ListNode reversedHead = reverseAndCloneList(head);
+        ListNode tempHead = head; // Use a temporary pointer to avoid modifying the original head pointer.
+
+        while (reversedHead != null && tempHead != null) {
+            if (reversedHead.val != tempHead.val) {
+                return false;
+            }
+            reversedHead = reversedHead.next;
+            tempHead = tempHead.next;
+        }
+
+        return true;
+    }
+
+    // Helper method to reverse and clone a linked list
+    private ListNode reverseAndCloneList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode newNode = new ListNode(current.val); // Create a new node with the current value
+            newNode.next = prev;
+            prev = newNode;
+            current = current.next;
+        }
+        return prev;
+    }
+}
